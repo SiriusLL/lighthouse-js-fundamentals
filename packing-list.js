967f62766128d1d05243d42cbc5b5eaae0982df0
SiriusLL <sirius.love.light@gmail.com>
2021-01-26 10:56:07 +0000

    learning arrays & looping through arrays

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ['bowls', 'plates', 'posts', 'pans', 'eating utensiles', 'glasses', 'cups', 'cooking utensiles'];
+
+console.log('Kitchen stuff to pack:');
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
